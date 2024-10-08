b95cd7e32e878028fc9ed23570320075e96ad60c
FEDERICO CURRAO - GQue - DEVELOPER <68132577+fcurrao@users.noreply.github.com>
2024-10-07 21:02:04 -0300

    Update Topbar.tsx

--- src/Components/Generals/Topbar.tsx
+++ src/Components/Generals/Topbar.tsx
@@ -47,7 +47,7 @@ const TopBar: React.FC<{ user: User, handleLogout: () => void }> = ({ user, hand
           <svg xmlns="http://www.w3.org/2000/svg" width='30' height='30' fill='white' viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z" /></svg>
           <span style={{ position: 'relative', top: '-5px' }} > {user.role}</span>
         </Typography>
-        <Button title="LOG OUT" sx={{ marginRight: '10px', position: 'absolute',right: '1px' }} variant="contained" color="warning" onClick={handleLogout}>
+        <Button title="LOG OUT" sx={{ marginRight: '10px', position: 'absolute',right: '1px' , backgroundColor:'#2e8b5770' }} variant="contained" onClick={handleLogout}>
           Log out
         </Button>
       </div>
